080bf2aa188d12f92fa4fa3daf8eb684d33b8540
endlessillusion <bborror@gmail.com>
2023-02-20 18:25:20 -0500

    More version updates

--- source/Marlin/Version.h
+++ source/Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "EndlessIllusion v1.0a"
+#define SHORT_BUILD_VERSION "EndlessIllusion v1.1"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2022-12-27"
+#define STRING_DISTRIBUTION_DATE "2023-02-20"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
